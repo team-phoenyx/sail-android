89944e061769319446e2f1cea62abc29d03f408c
DarkTerbear <terranceli.terbear@gmail.com>
2017-05-28 10:54:23 -0700

    Replaced hardcoded quotes w/ quotes API
    Version 0.94 Build 21
    New APK available (Uninstall before installing new APK)

--- app/src/main/java/io/phoenyx/sail/SplashActivity.java
+++ app/src/main/java/io/phoenyx/sail/SplashActivity.java
@@ -1,36 +1,17 @@
 package io.phoenyx.sail;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import io.phoenyx.sail.models.QuoteResponse;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class SplashActivity extends AppCompatActivity {
-    DBHandler dbHandler;
-    String quote;
-
-    SharedPreferences sharedPreferences;
-    InputStream inputStream;
-    BufferedReader bufferedReader;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        dbHandler = new DBHandler(this);
-
-        sharedPreferences = getSharedPreferences("io.phoenyx.sail", MODE_PRIVATE);
+        //TODO Check connection
 
         /*
         if (sharedPreferences.getBoolean("firstrun", true)) {
@@ -69,25 +50,7 @@ public class SplashActivity extends AppCompatActivity {
         }
         */
 
-        SailService sailService = RetrofitClient.getClient("quotes.rest/").create(SailService.class);
-        sailService.getQOD().enqueue(new Callback<QuoteResponse>() {
-            @Override
-            public void onResponse(Call<QuoteResponse> call, Response<QuoteResponse> response) {
-                if (response.isSuccessful()) {
-                    quote = response.body().getContents().getQuotes().get(0).getQuote() + "\n" + "    ";
-                } else {
-                    quote = "Sorry, quotes are currently unavailable";
-                }
-            }
-
-            @Override
-            public void onFailure(Call<QuoteResponse> call, Throwable t) {
-                quote = "Sorry, quotes are currently unavailable";
-            }
-        });
-
         Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
-        startIntent.putExtra("quote", quote);
         startActivity(startIntent);
         finish();
     }
